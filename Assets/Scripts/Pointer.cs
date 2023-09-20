36e33e682a7dd2ea2cdc0e8c879cacd6bc6cd5a6
NoahPladys <pladys.noah@gmail.com>
2023-09-20 15:36:37 +0200

    Prototype v1

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public Target target;
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    void Found()
+    {
+        Debug.Log("GEVONDE");
+    }
+}
